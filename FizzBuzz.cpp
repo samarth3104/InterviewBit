5262635b7c54bccfddc9b103d062bb0f17513e8a
samarth3104 <51475423+samarth3104@users.noreply.github.com>
2019-06-12 00:44:17 +0530

    Interview Bit -> FizzBuzz

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,1 +1,18 @@
+vector<string> Solution::fizzBuzz(int A) {
+    vector<string> v;
+    for(int i=1;i<=A;i++){
+        string s="";
+        if(i%3==0){
+            s=s+"Fizz";
+        }
+        if(i%5==0){
+            s=s+"Buzz";
+        }
+        if(i%3!=0 && i%5!=0){
+            s=s+to_string(i);
+        }
+        v.push_back(s);
+    }
+    return v;
+}
 
