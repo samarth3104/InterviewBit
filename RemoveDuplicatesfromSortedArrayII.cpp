fd218aa821379287f644b72a7ae562aec5889929
samarth3104 <51475423+samarth3104@users.noreply.github.com>
2019-06-20 17:08:14 +0530

    InterviewBit -> Remove Duplicates from Sorted Array II

---
+++ RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,16 @@
+
+int Solution::removeDuplicates(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    if(A.size()<3)
+        return A.size();
+    int j=2,n=A.size();
+    for(int i=2;i<n;i++){
+        if(A[i]!=A[j-2])
+            A[j++]=A[i];
+    }
+    return j;
+}
