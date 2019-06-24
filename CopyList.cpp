712fdc204a4ba7c65c2db3ad41606e1b9c920993
samarth3104 <51475423+samarth3104@users.noreply.github.com>
2019-06-25 01:12:01 +0530

    InterviewBit -> CopyList

---
+++ CopyList.cpp
@@ -0,0 +1,38 @@
+RandomListNode* Solution::copyRandomList(RandomListNode* A) {
+
+    if(A == NULL){
+        return NULL;
+    }
+
+    RandomListNode* head = A;
+    RandomListNode* cloned;
+    RandomListNode* newHead = new RandomListNode(head->label);
+    RandomListNode* curr = head;
+    RandomListNode* temp = newHead;
+
+    unordered_map<RandomListNode*, RandomListNode*> myMap;
+
+    myMap.insert({head, newHead});
+    curr = curr->next;
+    while(curr != NULL){
+        cloned = new RandomListNode(curr->label);
+        temp->next = cloned;
+        temp = temp->next;
+        myMap.insert({curr, cloned});
+        curr = curr->next;
+    }
+
+    curr = A;
+    temp = newHead;
+    while(curr != NULL){
+        if((myMap.find(curr->random)) != myMap.end()){
+            myMap[curr]->random = myMap[curr->random];
+        }
+        else{
+            myMap[curr]->random = NULL;
+        }
+        curr = curr->next;
+    }
+
+    return newHead;
+}
